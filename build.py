7e0e317fb8cc862badbb66e348d4a1143680793d
Stefan van Essen <git@stefan-van-essen.nl>
2018-12-23 15:22:16 +0100

    Build for F4SE v0.6.15 (game version v1.10.120)

--- build.py
+++ build.py
@@ -4,11 +4,11 @@ import os, sys
 BUILD_DIR        = 'build'
 PROJECT_DIR      = 'src'
 PLATFORM_TOOLSET = 'v140'
-F4SE_REVISION    = 'tags/v0.6.13'
+F4SE_REVISION    = 'tags/v0.6.15'
 
 # Run build tools
 buildOK = os.system('python tools/build-tools/build_plugin.py "{}" "{}" "{}" "{}"'
     .format(BUILD_DIR, PROJECT_DIR, PLATFORM_TOOLSET, F4SE_REVISION))
-    
+
 # Report result
 sys.exit(buildOK)
